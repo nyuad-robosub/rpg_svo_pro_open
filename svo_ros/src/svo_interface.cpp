19a5d6f6fa1758ffb4ed56184874f83f153eda46
nhamngocminhquan <nhamngocminhquan@gmail.com>
2022-09-04 19:15:14 +0400

    Update svo_interface.cpp, using ApproximateTime instead of ExactTime

--- svo_ros/src/svo_interface.cpp
+++ svo_ros/src/svo_interface.cpp
@@ -501,8 +501,10 @@ void SvoInterface::monoLoop()
 
 void SvoInterface::stereoLoop()
 {
-  typedef message_filters::sync_policies::ExactTime<sensor_msgs::Image, sensor_msgs::Image> ExactPolicy;
-  typedef message_filters::Synchronizer<ExactPolicy> ExactSync;
+  // typedef message_filters::sync_policies::ExactTime<sensor_msgs::Image, sensor_msgs::Image> ExactPolicy;
+  // typedef message_filters::Synchronizer<ExactPolicy> ExactSync;
+  typedef message_filters::sync_policies::ApproximateTime<sensor_msgs::Image, sensor_msgs::Image> ApproxPolicy;
+  typedef message_filters::Synchronizer<ApproxPolicy> ApproxSync;
 
   ros::NodeHandle nh(nh_, "image_thread");
   ros::CallbackQueue queue;
@@ -514,7 +516,9 @@ void SvoInterface::stereoLoop()
   image_transport::ImageTransport it(nh);
   image_transport::SubscriberFilter sub0(it, cam0_topic, 1, std::string("raw"));
   image_transport::SubscriberFilter sub1(it, cam1_topic, 1, std::string("raw"));
-  ExactSync sync_sub(ExactPolicy(5), sub0, sub1);
+  // ExactSync sync_sub(ExactPolicy(5), sub0, sub1);
+  // sync_sub.registerCallback(boost::bind(&svo::SvoInterface::stereoCallback, this, _1, _2));
+  ApproxSync sync_sub(ApproxSync(5), sub0, sub1);
   sync_sub.registerCallback(boost::bind(&svo::SvoInterface::stereoCallback, this, _1, _2));
 
   while(ros::ok() && !quit_)
